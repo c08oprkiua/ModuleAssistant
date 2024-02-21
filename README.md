c1b53a1e40c271e92b41b4ad9040394fd48a7327
c08oprkiua <85906487+c08oprkiua@users.noreply.github.com>
2024-02-20 21:51:24 -0700

    Initial work on transpiler

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # ModuleAssistant
  Helper tool for making engine modules in Godot (4)
 
-
 This is a little thingamabob I made in an evening to automate all the set, get, and bind functions needed to register variables in Godot C++ modules.
 
-UI is self explanatory, use this tool at your (code's) own risk
+I'm also working on adding a very rough GDScript to C++ translator for transfering GDScript prototypes to C++.
+
+I've tried to make the UI self explanatory, and **use this tool at your (code's) own risk**. The GDScript to C++ converter in particular is gonna give rough results, not miraculous usable ones.
--- scripts/gdscript_to_cpp.gd
+++ scripts/gdscript_to_cpp.gd
@@ -6,18 +6,28 @@ var class_inherits:String
 
 var cppfilestring:String
 
+const class_template:String = "class {class}: public {inherits}{"
+
 class var_type: 
 	extends Resource
 	var type
 	var name:String = ""
 	var data = null
 	
-	func _init(value:Variant):
-		type = type_string(typeof(value))
-		data = value
+	func _init(value:Variant = null):
+		if value == null:
+			return
+		else:
+			type = type_string(typeof(value))
+			data = value
 
 func _on_input_pressed():
-	pass # Replace with function body.
+	var filein:FileDialog = FileDialog.new()
+	filein.access = FileDialog.ACCESS_FILESYSTEM
+	filein.add_filter("*.gd", "GDScript files")
+	filein.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+	filein.use_native_dialog = true
+	add_child(filein)
 
 func handle_variable():
 	var thevar:var_type
